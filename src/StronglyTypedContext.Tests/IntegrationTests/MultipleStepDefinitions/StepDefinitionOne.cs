1a9a4bfb8d38c5f27fa83cbeedcaa278f8fe17d1
Kevin Holditch <kevholditch@hotmail.com>
2013-12-11 09:17:45 +0000

    Added new test for mutliple step definition classes sharing a context

---
+++ src/StronglyTypedContext.Tests/IntegrationTests/MultipleStepDefinitions/StepDefinitionOne.cs
@@ -0,0 +1,46 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace StronglyTypedContext.Tests.IntegrationTests.MultipleStepDefinitions
+{
+
+    public interface ISharedContext
+    {
+        int CustomerId { get; set; }
+    }
+
+    [Binding]
+    public class MultipleStepDefinitionsOne : BaseBinding
+    {
+        [ScenarioContext]
+        public virtual ISharedContext Context { get; set; }
+
+        [Given(@"I have a step definition file with a scenario context property")]
+        public void GivenIHaveAStepDefinitionFileWithAScenarioContextProperty()
+        {
+            // empty step
+        }
+
+        [When(@"I set the property in one step definition class")]
+        public void WhenISetThePropertyInOneStepDefinitionClass()
+        {
+            Context.CustomerId = 1234;
+        }
+
+
+    }
+
+    [Binding]
+    public class MultipleStepDefinitionsTwo : BaseBinding
+    {
+        [ScenarioContext]
+        public virtual ISharedContext Context { get; set; }
+
+        [Then(@"I can read the property in a different step definition class")]
+        public void ThenICanReadThePropertyInADifferentStepDefinitionClass()
+        {
+            Context.CustomerId.Should().Be(1234);
+        }
+
+    }
+}
---
+++ src/StronglyTypedContext.Tests/IntegrationTests/MultipleStepDefinitions/TwoStepDefinitions.feature
@@ -0,0 +1,8 @@
+﻿Feature: TwoStepDefinitions
+	Scenario context attributes work accross two step definition classes
+
+@mytag
+Scenario: Sharing automatic scenario context between step definition files
+	Given I have a step definition file with a scenario context property	
+	When I set the property in one step definition class
+	Then I can read the property in a different step definition class
